e022ac8baf23b5928f4011a08692964487e70640
khoahc <khoahc98@gmail.com>
2022-04-08 21:11:44 +0700

    code sorting for users list

--- LiziWebParent/LiziBackEnd/src/main/java/com/lizi/admin/user/UserController.java
+++ LiziWebParent/LiziBackEnd/src/main/java/com/lizi/admin/user/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -28,15 +29,19 @@ public class UserController {
 	
 	@GetMapping("/users")
 	public String listFirstPage(Model model) {
-		return listByPage(1, model);
+		return listByPage(1, model, "firstName", "asc");
 	}
 	
 	@GetMapping("/users/page/{pageNum}")
-	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model) {
+	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model,
+			@Param("sortField") String sortField, @Param("sortDir") String sortDir) {
 		if(pageNum < 1) 
 			pageNum = 1;
 		
-		Page<User> page = service.listByPage(pageNum);
+		System.out.println("sortField: " + sortField);
+		System.out.println("sortDir: " + sortDir);
+		
+		Page<User> page = service.listByPage(pageNum, sortField, sortDir);
 		List<User> listUsers = page.getContent();
 				
 		long totalItems = page.getTotalElements();
@@ -47,12 +52,17 @@ public class UserController {
 			endCount = totalItems;
 		}
 		
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc"; 
+				
 		model.addAttribute("currentPage", pageNum);
 		model.addAttribute("totalPages", page.getTotalPages());
 		model.addAttribute("startCount", startCount);
 		model.addAttribute("endCount", endCount);
 		model.addAttribute("totalItems", totalItems);
 		model.addAttribute("listUsers", listUsers);
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);		
+		model.addAttribute("reverseSortDir", reverseSortDir);
 		
 		return "users";
 	}
